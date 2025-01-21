106629e5822977deaeca91706ad8152122e4870c
Toshika <toshikagoswami4@gmail.com>
2025-01-21 13:48:36 +0000

    datatpes summary

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -7,4 +7,30 @@ let name = "toshika"
 let age = 20 
 let sex = "female"
 
+/* #Dynamically typed languages: Check variable types at runtime, while the code is running.
+Statically typed languages: Check variable types at compile time, before the code is run.
+JS is a dynamically typed language 
+
+   #types of datatypes(depending on the memory allocation)  -->  primitive and non primitives
+ 1. primitive : 7 categories ( string , number, boolean , undefined , symbol, BigInt, null  )
+                to declare a symbol- 
+                    constId=Symbol("123")              
+
+    if you check the data type of null it will say object 
+
+ 2. non primitive or refrence type (direct ref. can be allocated in the memory) : 
+ *Array-
+    const heores = [1,2,3,4,5]
+*object-
+    let myObject{
+    name:"toshika",
+    age:22
+    }
+*function-
+    const myFunction = funtion(){
+    console.log("hello-world-yara")
+    }
+
+ */
+
 
